b10776f01b3070903916fdbec4310ec77e534d11
Félix Dion Robidoux <felix@fdrobidoux.com>
2019-06-06 19:36:43 -0400

    Removed private keywords.

--- MonoTycoon.Core/Common/TimerTask.cs
+++ MonoTycoon.Core/Common/TimerTask.cs
@@ -3,77 +3,70 @@ using Microsoft.Xna.Framework;
 
 namespace MonoTycoon.Core.Common
 {
-    public sealed class TimerTask : IUpdateable
-    {
-        private readonly Action _performTask;
-        private double _intervalMs;
-        private bool _recurring;
-        private bool _isFinished;
-        private double _elapsedMs;
+	public sealed class TimerTask : IUpdateable
+	{
+		readonly Action _performTask;
+		double _intervalMs;
 
-        public bool Enabled { get; set; }
-        public int UpdateOrder => 0;
+		public bool Enabled { get; set; }
+		public int UpdateOrder => 0;
 
-        [Obsolete("Not used for this component.")]
-        public event EventHandler<EventArgs> EnabledChanged;
-        [Obsolete("Not used for this component.")]
-        public event EventHandler<EventArgs> UpdateOrderChanged;
+		[Obsolete("Not used for this component.")]
+		public event EventHandler<EventArgs> EnabledChanged;
+		[Obsolete("Not used for this component.")]
+		public event EventHandler<EventArgs> UpdateOrderChanged;
 
-        public double IntervalMs
-        {
-            get => _intervalMs;
-            set
-            {
-                _intervalMs = value;
-                Reset();
-            }
-        }
-        public bool Recurring
-        {
-            get => _recurring;
-            set => _recurring = value;
-        }
-        public bool IsFinished => _isFinished;
-        public double ElapsedMs => _elapsedMs;
+		public double IntervalMs
+		{
+			get => _intervalMs;
+			set
+			{
+				_intervalMs = value;
+				Reset();
+			}
+		}
+		public bool Recurring { get; set; }
+		public bool IsFinished { get; private set; }
+		public double ElapsedMs { get; private set; }
 
-        public TimerTask(Action performTask, double intervalMs, bool recurring = true)
-        {
-            _performTask = performTask;
-            _intervalMs = intervalMs;
-            _recurring = recurring;
-        }
+		public TimerTask(Action performTask, double intervalMs, bool recurring = true)
+		{
+			_performTask = performTask;
+			_intervalMs = intervalMs;
+			Recurring = recurring;
+		}
 
-        public void Update(TimeSpan delta)
-        {
-            if (!Enabled)
-                return;
+		public void Update(TimeSpan delta)
+		{
+			if (!Enabled)
+				return;
 
-            if (_isFinished)
-                return;
+			if (IsFinished)
+				return;
 
-            _elapsedMs += delta.TotalMilliseconds;
-            if (!(_elapsedMs > _intervalMs))
-                return;
+			ElapsedMs += delta.TotalMilliseconds;
+			if (!(ElapsedMs > _intervalMs))
+				return;
 
-            _performTask();
-            _isFinished = true;
-            if (_recurring)
-                Reset();
-        }
+			_performTask();
+			IsFinished = true;
+			if (Recurring)
+				Reset();
+		}
 
-        public void Update(GameTime gameTime) => Update(gameTime.ElapsedGameTime);
+		public void Update(GameTime gameTime) => Update(gameTime.ElapsedGameTime);
 
-        /// <summary>
-        ///     Resets the timer, and sets the timer as unfinished.
-        /// </summary>
-        /// <param name="modEnabled">A <see langword="bool"/> if it's necessary to change enabled status, or <see langword="null"/> if otherwise.</param>
-        public void Reset(bool? modEnabled = null)
-        {
-            _elapsedMs = 0;
-            _isFinished = false;
+		/// <summary>
+		///     Resets the timer, and sets the timer as unfinished.
+		/// </summary>
+		/// <param name="modEnabled">A <see langword="bool"/> if it's necessary to change enabled status, or <see langword="null"/> if otherwise.</param>
+		public void Reset(bool? modEnabled = null)
+		{
+			ElapsedMs = 0;
+			IsFinished = false;
 
-            if (modEnabled is bool en) 
-                Enabled = en;
-        }
-    }
+			if (modEnabled is bool en)
+				Enabled = en;
+		}
+	}
 }

f435c13237ed3e9d335d3500a36f5b0de50c149a
Félix Dion Robidoux <felix@fdrobidoux.com>
2019-06-14 11:34:36 -0400

    Readded State handling files to MonoTycoon.Core

---
+++ MonoTycoon.Core/States/IStateSensitive.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+
+namespace MonoTycoon.Core.States
+{
+    public interface IStateSensitive<TSystem, in TEnum>
+        where TSystem : IGameComponent
+        where TEnum : Enum
+    {
+        void StateChanged(TSystem component, TEnum previousState);
+    }
+}
